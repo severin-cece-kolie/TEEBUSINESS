f0bbdd115202c4559944d5d02936ebc3c6e0ee86
sévérin cécé KOLIE <severincecekolie05@gmail.com>
2026-06-11 03:01:10 +0000

    env

--- teebusiness_core/settings.py
+++ teebusiness_core/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-change-this-in-production-use-get-random-secret-key')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'odsa&9q%#ic#7q6kwumdl2o=v9(wy1f96ij%6vi2&%)vbouc@s')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = os.environ.get('DEBUG', 'False') == 'False'
 
 # Raise error if DEBUG is True in production-like environment
 if not DEBUG and not os.environ.get('SECRET_KEY'):
